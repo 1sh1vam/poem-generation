de5e06a423976f52f44f1c5f550c7fc3af957fcf
1sh1vam <shivam07.mt@gmail.com>
2023-12-20 23:33:58 +0530

    feat: build setup for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: process.env.ORIGIN_HOST,
         methods: ['GET', 'POST']
     }
 });
